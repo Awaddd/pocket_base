edbfb3e617951fe25c5345a68b4a1ef96a4561e2
Awad <34748292+Awaddd@users.noreply.github.com>
2023-07-23 00:19:25 +0100

    add pocketbase sdk

--- lib/state/state.dart
+++ lib/state/state.dart
@@ -1,8 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path/path.dart';
 import 'package:pocket_base/data/dog.dart';
+import 'package:pocketbase/pocketbase.dart';
 import 'package:sqflite/sqflite.dart';
 
+// pocketbase provider
+final pocketbase = Provider((ref) {
+  const url = 'http://10.0.2.2:8090';
+  return PocketBase(url);
+});
+
+// local sqflite provider
 final databaseProvider = Provider((ref) async {
   return openDatabase(
     join(await getDatabasesPath(), 'pb_demo.db'),
